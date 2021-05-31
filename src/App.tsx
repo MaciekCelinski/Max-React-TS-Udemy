bd7efdd501c0bad5cb6643ead07cac349cc91a4a
MaciekCelinski <mac.celinski@gmail.com>
2021-05-31 14:06:30 +0200

    Data Model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+  ];
+
   return (
     <div className="App">
-      <Todos items={['Learn React', 'Learn TS']} />
+      <Todos items={todos} />
     </div>
   );
 }
